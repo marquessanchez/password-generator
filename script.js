95a58c7a4b49e36c8edd3b7ea549b63629b32bc1
mrican217@gmail.com <mrican217@gmail.com>
2022-03-30 22:24:01 -0400

    functions and variables created ready to generate password

--- script.js
+++ script.js
@@ -1,44 +1,102 @@
 // Assignment code here
-var getCharacters;
-var getLowercase;
-var getUppercase;
-var getNumbers;
-var getSpecialCharacters;
-var getChoices;
+// var useCharacters;
+// var useLowercase;
+// var useUppercase;
+// var useNumbers;
+// var specialCharacters = '!@#$%^&*()={}[]/?.,'
+// var useChoices;
+// var userChoice;
 
 
 // Ask for password choices
-function getCharacters(){
+    // var useCharacters= window.prompt("Enter a number from 8-126")
+    // if (userChoice <= 7) {
+    //     window.prompt("Enter a number from 8-126");
+    // } else if 
+    // (userChoice >= 125) {
+    //     window.prompt("Enter a number from 8-126");
+    // } else {
+    //     userChoice = useCharacters;
+    // }
+
+    var lowercase = window.prompt("Would you like to use lowercase letters? Enter Y for YES")
+    if (userChoice = "Y") {
+        userChoice = useLowercase();
+        console.log("lowercase")
+    }
+    else {
+        userChoice != useLowercase;
+    }
+    
+    var uppercase = window.prompt("Would you like to use uppercase letters? Enter Y for YES")
+    if (userChoice = "Y") {
+        userChoice = useUppercase();
+        console.log("uppercase")
+    }
+    else {
+        userChoice != useUppercase();
+    };
+
+    var numbers= window.prompt("Would you like to use numbers? Enter Y for YES")
+    if (userChoice = "Y") {
+        userChoice = useNumbers();
+        console.log("numbers")
+    }
+    else {
+        userChoice != useNumbers();;
+    }
+
+    var specials= window.prompt("Would you like to use special characters? Enter Y for YES")
+    if (userChoice = "Y") {
+        userChoice = useSpecials();
+        console.log("special characters")
+    }
+    else {
+        userChoice != useSpecials();
+    }
 
 
+
+// }
+
+// for (i = 0; i < 7; i++);
+
+function useCharacters(){
+    return useCharacters.length;
 }
 
-function getLowercae(){
-    return String.fromCharCode(Math.floor.random() * 26 +97);
+function useLowercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-function getUppercase(){
-    return String.fromCharCode(Math.floor.Math.random() * 26 +65);
+function useUppercase() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function getNumbers(){
-    return String.fromCharCode(Math.floor.Math.random() * 10 + 48);
-
+function useNumbers() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-function getSpecialCharacters(){
-    var specialCharacters = '!@#$%^&*()={}[]/?.,'
-    return String.fromCharCode(Math.floor.Math.random() * 10 + 48);
-
-
+function useSpecials() {
+    var specials = '~!@#$%^&*()_+-={}[]:;,./<>?'
+    return specials [Math.floor(Math.random() * specials.length)];
 }
 
+// var usePasswordLength = function(){
+//     passwordLength = "";
 
-// Get references to the #generate element
+// }
+
+// use references to the #generate element
 var generateBtn = document.querySelector("#generate");
+console.log('clicked');
 
 function generatePassword() {
-    console.log("Here's your password");
+    userChoice = password;
+    if (password.lenth === length) {
+        return password;
+    }
+    // console.log("Here's your password");
 }
 
 
@@ -55,4 +113,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-console.log (password);
+// console.log ("i am being clicked");
+
