736dcae50d9b971eff3e5db63445ef7632427629
mrican217@gmail.com <mrican217@gmail.com>
2022-03-31 00:26:42 -0400

    function to get password creted... not yet working

--- script.js
+++ script.js
@@ -9,96 +9,91 @@
 
 
 // Ask for password choices
-    // var useCharacters= window.prompt("Enter a number from 8-126")
-    // if (userChoice <= 7) {
-    //     window.prompt("Enter a number from 8-126");
-    // } else if 
-    // (userChoice >= 125) {
-    //     window.prompt("Enter a number from 8-126");
-    // } else {
-    //     userChoice = useCharacters;
-    // }
+function userChoice() {
+    var characterLength = window.prompt("Enter a number from 8-126")
+    if (characterLength <= 7) {
+        window.prompt("Enter a number from 8-126");
+    } else if 
+    (characterLength >= 129) {
+        window.prompt("Enter a number from 8-126");
+    } else {
+        characterLength = useCharacters;
+    }
 
     var lowercase = window.prompt("Would you like to use lowercase letters? Enter Y for YES")
-    if (userChoice = "Y") {
-        userChoice = useLowercase();
+    if (lowercase = true) {
+        useLowercase();
         console.log("lowercase")
     }
     else {
-        userChoice != useLowercase;
+        lowercase != useLowercase;
     }
     
     var uppercase = window.prompt("Would you like to use uppercase letters? Enter Y for YES")
-    if (userChoice = "Y") {
-        userChoice = useUppercase();
+    if (uppercase = true) {
+        useUppercase();
         console.log("uppercase")
     }
     else {
-        userChoice != useUppercase();
+        uppercase != useUppercase();
     };
 
     var numbers= window.prompt("Would you like to use numbers? Enter Y for YES")
-    if (userChoice = "Y") {
-        userChoice = useNumbers();
+    if (numbers = true) {
+        useNumbers();
         console.log("numbers")
     }
     else {
-        userChoice != useNumbers();;
+        numbers != useNumbers();;
     }
 
     var specials= window.prompt("Would you like to use special characters? Enter Y for YES")
-    if (userChoice = "Y") {
-        userChoice = useSpecials();
+    if (specials = true) {
+        useSpecials();
         console.log("special characters")
     }
     else {
-        userChoice != useSpecials();
+        specials != useSpecials();
     }
-
-
-
+}
+// function generatePassword(){
+//     var generatePassword = userChoice();
 // }
 
-// for (i = 0; i < 7; i++);
-
-function useCharacters(){
-    return useCharacters.length;
-}
-
-function useLowercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function useUppercase() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function useNumbers() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function useSpecials() {
-    var specials = '~!@#$%^&*()_+-={}[]:;,./<>?'
-    return specials [Math.floor(Math.random() * specials.length)];
-}
-
-// var usePasswordLength = function(){
-//     passwordLength = "";
-
-// }
+    
+    function useCharacters(){
+        return characterLength;
+    }
+    
+    function useLowercase() {
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
+    
+    function useUppercase() {
+        return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    }
+    
+    function useNumbers() {
+        return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    }
+    
+    function useSpecials() {
+        var specials = '~!@#$%^&*()_+-={}[]:;,./<>?'
+        return specials [Math.floor(Math.random() * specials.length)];
+    }
+    var generatePassword = function() {
+        userChoice();
+    }
 
 // use references to the #generate element
 var generateBtn = document.querySelector("#generate");
-console.log('clicked');
 
-function generatePassword() {
-    userChoice = password;
-    if (password.lenth === length) {
-        return password;
-    }
-    // console.log("Here's your password");
+
+var password = ""
+for (i=0; i<useCharacters; i++){
+    var characters = userChoice();
+    password = password + characters;
 }
-
 
 // Write password to the #password input
 function writePassword() {
@@ -109,9 +104,6 @@ function writePassword() {
 
 }
 
-
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-// console.log ("i am being clicked");
 
